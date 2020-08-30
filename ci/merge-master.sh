d5667f2a94732c00701bd762626ad944ff1f2bb7
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-08-30 20:21:59 +0900

    ci: fix merge

--- ci/merge-master.sh
+++ ci/merge-master.sh
@@ -9,9 +9,12 @@ if [ "$DRONE_SOURCE_BRANCH" = master ]; then
 fi
 
 if [ "${DRONE_BUILD_EVENT}" = pull_request ]; then
-  ghc exec -- run-ci merge
-else
-  ghc exec -- run-ci merge --base master
+  # By default Drone merges the base branch if the event type is pull request
+  ghc exec -- git log -n 5 --pretty=oneline
+  exit 0
 fi
+
+ghc exec -- ghc exec -- git fetch origin master
+ghc exec -- env GIT_AUTHOR_NAME=ci-helper GIT_AUTHOR_EMAIL=ci-helper@example.com GIT_COMMITTER_NAME=ci-helper GIT_COMMITTER_EMAIL=ci-helper@example.com git merge --no-edit origin/master
 
 ghc exec -- git log -n 5 --pretty=oneline
